74f8dfc3718e6ffd613e29023c4b8fd37a83a1f8
Tavis Palmer <tavis@tavispalmer.com>
2025-07-20 12:06:53 -0400

    Add windows build support (not tested yet)

--- stbi/build.rs
+++ stbi/build.rs
@@ -1,58 +1,89 @@
-use std::{env, path::PathBuf, process::Command};
+use std::{env, ffi::{OsStr, OsString}, path::PathBuf, process::Command};
+
+fn concat<A: AsRef<OsStr>, B: AsRef<OsStr>>(a: A, b: B) -> OsString {
+    let a = a.as_ref();
+    let b = b.as_ref();
+    let mut ret = OsString::with_capacity(a.len() + b.len());
+    ret.push(a);
+    ret.push(b);
+    ret
+}
 
 fn main() {
     let out = &PathBuf::from(env::var_os("OUT_DIR").unwrap());
+
+    #[cfg(target_os = "windows")]
+    if Command::new("cl").arg("/?").status().is_ok_and(|status| status.success()) {
+        assert!(Command::new("cl")
+            .arg("/MD")
+            .arg("/nologo")
+            .arg("/showIncludes")
+            .arg("/utf-8")
+            .arg("/W2")
+            .arg("/O2")
+            .arg("/Gw")
+            .arg(concat("/Fd", out.join("app.pdb")))
+            .arg(concat("/Fo", out.join("app.obj")))
+            .arg("/c")
+            .arg("stb_image.c")
+            .status()
+            .unwrap()
+            .success());
+
+        assert!(Command::new("lib")
+            .arg("/NOLOGO")
+            .arg("/MACHINE:x86")
+            .arg(concat("/OUT:", out.join("libapp.a")))
+            .arg(out.join("app.obj"))
+            .status()
+            .unwrap()
+            .success());
+    }
     
-    // these args are from running meson + ninja
-    assert!(Command::new(env::var_os("CC").unwrap_or("cc".into()))
-        .arg("-Isrc")
-        .arg("-fdiagnostics-color=always")
-        .arg("-Wall")
-        .arg("-Winvalid-pch")
-        .arg("-O3") // we always compile stb_image.h in release mode
+    #[cfg(not(target_os = "windows"))]
+    if Command::new("cc").arg("--version").status().is_ok_and(|status| status.success()) {
+        // these args are from running meson + ninja
+        assert!(Command::new("cc")
+            .arg("-fdiagnostics-color=always")
+            .arg("-Wall")
+            .arg("-Winvalid-pch")
+            .arg("-O3") // we always compile stb_image.h in release mode
+            .arg("-MD")
+            .arg("-MQ")
+            .arg(out.join("stb_image.o"))
+            .arg("-MF")
+            .arg(out.join("stb_image.o.d"))
+            .arg("-o")
+            .arg(out.join("stb_image.o"))
+            .arg("-c")
+            .arg("stb_image.c")
+            .status()
+            .unwrap()
+            .success());
 
-        // we use callbacks to use rust's stdio instead
-        .arg("-DSTBI_NO_STDIO")
-        // define STB_IMAGE_IMPLEMENTATION so we get the function definitions
-        .arg("-DSTB_IMAGE_IMPLEMENTATION")
-        // need to define c language explicitly, since we're compiling a header file
-        .arg("-xc")
+        // create library
+        assert!(Command::new("rm")
+            .arg("-f")
+            .arg(out.join("libstbi.a"))
+            .status()
+            .unwrap()
+            .success());
 
-        .arg("-MD")
-        .arg("-MQ")
-        .arg(out.join("stb_image.o"))
-        .arg("-MF")
-        .arg(out.join("stb_image.o.d"))
-        .arg("-o")
-        .arg(out.join("stb_image.o"))
-        .arg("-c")
-        .arg("stb_image.h")
-        .status()
-        .unwrap()
-        .success());
+        assert!(Command::new("ar")
+            .arg("csr")
+            .arg(out.join("libstbi.a"))
+            .arg(out.join("stb_image.o"))
+            .status()
+            .unwrap()
+            .success());
 
-    // create library
-    assert!(Command::new("rm")
-        .arg("-f")
-        .arg(out.join("libstbi.a"))
-        .status()
-        .unwrap()
-        .success());
-
-    assert!(Command::new("ar")
-        .arg("csr")
-        .arg(out.join("libstbi.a"))
-        .arg(out.join("stb_image.o"))
-        .status()
-        .unwrap()
-        .success());
-
-    assert!(Command::new("ranlib")
-        .arg("-c")
-        .arg(out.join("libstbi.a"))
-        .status()
-        .unwrap()
-        .success());
+        assert!(Command::new("ranlib")
+            .arg("-c")
+            .arg(out.join("libstbi.a"))
+            .status()
+            .unwrap()
+            .success());
+    }
 
     // link with stbi
     println!("cargo:rustc-link-search={}", out.display());
---
+++ stbi/stb_image.c
@@ -0,0 +1,3 @@
+#define STBI_NO_STDIO
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
